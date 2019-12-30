20f209bd04faa71786ab8bdbd370bb1ab883225b
Joe Elliott <number101010@gmail.com>
2019-12-29 21:44:18 -0500

    Added some RHS lex tests
    Signed-off-by: Joe Elliott <number101010@gmail.com>

--- processor/streamprocessor/traceql/lex_test.go
+++ processor/streamprocessor/traceql/lex_test.go
@@ -8,7 +8,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// jpe - add RHS tests
 func TestLex(t *testing.T) {
 	for _, tc := range []struct {
 		input    string
@@ -21,13 +20,13 @@ func TestLex(t *testing.T) {
 		{` spans{ name < "test" } `, []int{STREAM_TYPE_SPANS, OPEN_BRACE, FIELD_NAME, LT, STRING, CLOSE_BRACE}},
 		{` spans{ name <= "test" } `, []int{STREAM_TYPE_SPANS, OPEN_BRACE, FIELD_NAME, LTE, STRING, CLOSE_BRACE}},
 		{` spans{ name =~ "test" } `, []int{STREAM_TYPE_SPANS, OPEN_BRACE, FIELD_NAME, RE, STRING, CLOSE_BRACE}},
-		{` spans{ name !~ "test" } `, []int{STREAM_TYPE_SPANS, OPEN_BRACE, FIELD_NAME, NRE, STRING, CLOSE_BRACE}},
+		{` spans{ "test" !~ name } `, []int{STREAM_TYPE_SPANS, OPEN_BRACE, STRING, NRE, FIELD_NAME, CLOSE_BRACE}},
 		{` spans{ name <= 13 } `, []int{STREAM_TYPE_SPANS, OPEN_BRACE, FIELD_NAME, LTE, INTEGER, CLOSE_BRACE}},
 		{` spans{ duration <= 13 } `, []int{STREAM_TYPE_SPANS, OPEN_BRACE, FIELD_DURATION, LTE, INTEGER, CLOSE_BRACE}},
 		{` spans{ duration <= 1.21 } `, []int{STREAM_TYPE_SPANS, OPEN_BRACE, FIELD_DURATION, LTE, FLOAT, CLOSE_BRACE}},
 		{` spans{ atts.thing <= 13 } `, []int{STREAM_TYPE_SPANS, OPEN_BRACE, FIELD_ATTS, DOT, IDENTIFIER, LTE, INTEGER, CLOSE_BRACE}},
 		{` spans{ events.thing <= 13 } `, []int{STREAM_TYPE_SPANS, OPEN_BRACE, FIELD_EVENTS, DOT, IDENTIFIER, LTE, INTEGER, CLOSE_BRACE}},
-		{` spans{ status.code <= 13 } `, []int{STREAM_TYPE_SPANS, OPEN_BRACE, FIELD_STATUS, DOT, FIELD_CODE, LTE, INTEGER, CLOSE_BRACE}},
+		{` spans{ 13 <= status.code } `, []int{STREAM_TYPE_SPANS, OPEN_BRACE, INTEGER, LTE, FIELD_STATUS, DOT, FIELD_CODE, CLOSE_BRACE}},
 		{` spans{ status.message <= 13 } `, []int{STREAM_TYPE_SPANS, OPEN_BRACE, FIELD_STATUS, DOT, FIELD_MSG, LTE, INTEGER, CLOSE_BRACE}},
 		{` spans{ parent*.duration <= 13 } `, []int{STREAM_TYPE_SPANS, OPEN_BRACE, FIELD_DESCENDANT, DOT, FIELD_DURATION, LTE, INTEGER, CLOSE_BRACE}},
 		{` spans{ parent.duration <= 13 } `, []int{STREAM_TYPE_SPANS, OPEN_BRACE, FIELD_PARENT, DOT, FIELD_DURATION, LTE, INTEGER, CLOSE_BRACE}},
@@ -35,6 +34,7 @@ func TestLex(t *testing.T) {
 		{` spans{ process.name <= 13 } `, []int{STREAM_TYPE_SPANS, OPEN_BRACE, FIELD_PROCESS, DOT, FIELD_NAME, LTE, INTEGER, CLOSE_BRACE}},
 		{` traces{ process.name <= 13 } `, []int{STREAM_TYPE_TRACES, OPEN_BRACE, FIELD_PROCESS, DOT, FIELD_NAME, LTE, INTEGER, CLOSE_BRACE}},
 		{` traces{ isRoot = 1 } `, []int{STREAM_TYPE_TRACES, OPEN_BRACE, FIELD_IS_ROOT, EQ, INTEGER, CLOSE_BRACE}},
+		{` traces{ parent*.duration =~ events.blerg } `, []int{STREAM_TYPE_TRACES, OPEN_BRACE, FIELD_DESCENDANT, DOT, FIELD_DURATION, RE, FIELD_EVENTS, DOT, IDENTIFIER, CLOSE_BRACE}},
 	} {
 		t.Run(tc.input, func(t *testing.T) {
 			actual := []int{}
