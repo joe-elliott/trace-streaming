0abcd9fc3add4539b2d838f9b01b3efd321974d8
Joe Elliott <number101010@gmail.com>
2020-01-06 16:30:18 -0500

    Added tsp
    Signed-off-by: Joe Elliott <number101010@gmail.com>

---
+++ cmd/tsp/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"log"
+
+	"github.com/open-telemetry/opentelemetry-collector/defaults"
+	"github.com/open-telemetry/opentelemetry-collector/processor"
+	"github.com/open-telemetry/opentelemetry-collector/service"
+
+	"github.com/joe-elliott/trace-streaming/processor/streamprocessor"
+)
+
+func main() {
+	handleErr := func(err error) {
+		if err != nil {
+			log.Fatalf("Failed to run the service: %v", err)
+		}
+	}
+
+	factories, err := defaults.Components()
+	handleErr(err)
+
+	info := service.ApplicationStartInfo{
+		ExeName:  "tsp",
+		LongName: "trace stream processing",
+		Version:  "x.x.x.x",
+		GitHash:  "",
+	}
+
+	customProcessors, err := processor.Build(
+		&streamprocessor.Factory{},
+	)
+	handleErr(err)
+
+	for k, v := range customProcessors {
+		factories.Processors[k] = v
+	}
+
+	svc, err := service.New(factories, info)
+	handleErr(err)
+
+	err = svc.Start()
+	handleErr(err)
+}
